485598d3826ffcda06547bb0fe8808aa1bb70fc4
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-02-04 18:44:42 +1100

    Define basic commands

--- robot.js
+++ robot.js
@@ -1,4 +1,34 @@
 class Robot {
+  constructor() {
+    this.x = null;
+    this.y = null;
+    this.facing = null;
+  }
+
+  PLACE(x, y, f) {
+    // TODO: validate input
+    this.x = x;
+    this.y = y;
+    this.facing = f;
+  }
+
+  MOVE() {
+    if (this.facing === 'NORTH' && this.y < 4) this.y++;
+    if (this.facing === 'SOUTH' && this.y > 0) this.y--;
+    if (this.facing === 'EAST' && this.x < 4) this.x++;
+    if (this.facing === 'WEST' && this.x > 0) this.x--;
+  }
+
+  REPORT() {
+    // Expects a PLACE command to have been called first
+    if (!this.x && !this.y && !this.facing) {
+      throw 'a PLACE command is required first'
+    }
+    return [this.x, this.y, this.facing];
+  }
+}
+
+const validatePlaceInput = (x, y, f) => {
 
 }
 
--- robot.test.js
+++ robot.test.js
@@ -1,5 +1,30 @@
 const Robot = require('./robot');
 
-test('Hello, world', () => {
-  expect('Hello, world').toBe('Hello, world');
+test('REPORT should return the robot\'s current position', () => {
+  const robot = new Robot();
+  robot.PLACE(0, 0, 'NORTH');
+
+  const expected = [0, 0, 'NORTH'];
+  const actual = robot.REPORT();
+
+  expect(actual).toEqual(expected);
+});
+
+test('PLACE should be the first command', () => {
+  const robot = new Robot();
+
+  expect(() => {
+    robot.REPORT();
+  }).toThrowError();
+});
+
+test('MOVE should move the robot one unit north', () => {
+  const robot = new Robot();
+  robot.PLACE(0, 0, 'NORTH');
+  robot.MOVE();
+
+  const expected = [0, 1, 'NORTH'];
+  const actual = robot.REPORT();
+
+  expect(actual).toEqual(expected);
 });
