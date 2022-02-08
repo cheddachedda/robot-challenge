4bbe2303269b96cd05543c56390645e663193d66
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-02-09 10:02:18 +1100

    Wrap tests in describe blocks

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -1,7 +1,8 @@
 const { generateID, reduceInput, validateInput } = require('./helpers');
 
 describe('generateID', () => {
-  test('generateID() should produce a valid ID', () => {
+
+  test('should produce a valid ID', () => {
     const id = generateID();
     const regex = /[A-Z]{2}\d{3}/;
   
@@ -13,109 +14,118 @@ describe('generateID', () => {
     const actualLength = id.length;
     expect(actualLength).toBe(expectedlength);
   });
+
 });
 
-test('reduceInput() should trim whitespace', () => {
-  const tests = [
-    ' INPUT ',
-    '  INPUT',
-    'INPUT  ',
-    '\nINPUT  ',
-    ' INPUT  \n',
-  ];
+describe('reduceInput()', () => {
 
-  tests.forEach((test) => {
-    const expected = 'INPUT';
-    const actual = reduceInput(test);
-    expect(actual).toBe(expected);
+  test('should trim whitespace', () => {
+    const tests = [
+      ' INPUT ',
+      '  INPUT',
+      'INPUT  ',
+      '\nINPUT  ',
+      ' INPUT  \n',
+    ];
+  
+    tests.forEach((test) => {
+      const expected = 'INPUT';
+      const actual = reduceInput(test);
+      expect(actual).toBe(expected);
+    });
   });
-});
-
-test('reduceInput() should convert to uppercase', () => {
-  const tests = [
-    'input',
-    'INPUT',
-    'inPut',
-    'Input'
-  ];
-
-  tests.forEach((test) => {
-    const expected = 'INPUT';
-    const actual = reduceInput(test);
-    expect(actual).toBe(expected);
+  
+  test('should convert to uppercase', () => {
+    const tests = [
+      'input',
+      'INPUT',
+      'inPut',
+      'Input'
+    ];
+  
+    tests.forEach((test) => {
+      const expected = 'INPUT';
+      const actual = reduceInput(test);
+      expect(actual).toBe(expected);
+    });
   });
+
 });
 
-test('should return a valid boolean', () => {
-  const expected = 'boolean';
-  const actual = typeof validateInput(-1, 5, 0).valid;
+describe('validateInput()', () => {
 
-  expect(actual).toEqual(expected);
-});
-
-test('should return an errors object', () => {
-  const expected = 'object';
-  const actual = typeof validateInput(-1, 5, 0).errors;
-
-  expect(actual).toEqual(expected);
-});
-
-test('x should be a number between 0 and 4', () => {
-  const tests = [
-    { x: 0, valid: true },
-    { x: 4, valid: true },
-    { x: -1, valid: false },
-    { x: 5, valid: false },
-    { x: null, valid: false },
-    { x: '2', valid: false },
-    { x: 1.5, valid: false },
-    { x: [1], valid: false },
-  ];
-
-  tests.forEach((test) => {
-    const expected = test.valid;
-    const actual = validateInput(test.x, 0, 'NORTH').valid;
-    expect(actual).toBe(expected);
+  test('should return a valid boolean', () => {
+    const expected = 'boolean';
+    const actual = typeof validateInput(-1, 5, 0).valid;
+  
+    expect(actual).toEqual(expected);
   });
-});
-
-test('y should be a number between 0 and 4', () => {
-  const tests = [
-    { y: 0, valid: true },
-    { y: 4, valid: true },
-    { y: -1, valid: false },
-    { y: 5, valid: false },
-    { y: null, valid: false },
-    { y: '2', valid: false },
-    { y: 1.5, valid: false },
-    { y: [1], valid: false },
-  ];
-
-  tests.forEach((test) => {
-    const expected = test.valid;
-    const actual = validateInput(0, test.y, 'NORTH').valid;
-    expect(actual).toBe(expected);
+  
+  test('should return an errors object', () => {
+    const expected = 'object';
+    const actual = typeof validateInput(-1, 5, 0).errors;
+  
+    expect(actual).toEqual(expected);
   });
-});
-
-test('f should be a valid cardinal direction', () => {
-  const tests = [
-    { f: 'NORTH', valid: true },
-    { f: 'SOUTH', valid: true },
-    { f: 'EAST', valid: true },
-    { f: 'WEST', valid: true },
-    { f: 'NORD', valid: false },
-    { f: 'S', valid: false },
-    { f: 'NORTHEAST', valid: false },
-    { f: 'SOUTH WEST', valid: false },
-    { f: 0, valid: false },
-    { f: 'UP', valid: false },
-    { f: true, valid: false },
-  ];
-
-  tests.forEach((test) => {
-    const expected = test.valid;
-    const actual = validateInput(0, 0, test.f).valid;
-    expect(actual).toBe(expected);
+  
+  test('x should be a number between 0 and 4', () => {
+    const tests = [
+      { x: 0, valid: true },
+      { x: 4, valid: true },
+      { x: -1, valid: false },
+      { x: 5, valid: false },
+      { x: null, valid: false },
+      { x: '2', valid: false },
+      { x: 1.5, valid: false },
+      { x: [1], valid: false },
+    ];
+  
+    tests.forEach((test) => {
+      const expected = test.valid;
+      const actual = validateInput(test.x, 0, 'NORTH').valid;
+      expect(actual).toBe(expected);
+    });
   });
+  
+  test('y should be a number between 0 and 4', () => {
+    const tests = [
+      { y: 0, valid: true },
+      { y: 4, valid: true },
+      { y: -1, valid: false },
+      { y: 5, valid: false },
+      { y: null, valid: false },
+      { y: '2', valid: false },
+      { y: 1.5, valid: false },
+      { y: [1], valid: false },
+    ];
+  
+    tests.forEach((test) => {
+      const expected = test.valid;
+      const actual = validateInput(0, test.y, 'NORTH').valid;
+      expect(actual).toBe(expected);
+    });
+  });
+  
+  test('f should be a valid cardinal direction', () => {
+    const tests = [
+      { f: 'NORTH', valid: true },
+      { f: 'SOUTH', valid: true },
+      { f: 'EAST', valid: true },
+      { f: 'WEST', valid: true },
+      { f: 'NORD', valid: false },
+      { f: 'S', valid: false },
+      { f: 'NORTHEAST', valid: false },
+      { f: 'SOUTH WEST', valid: false },
+      { f: 0, valid: false },
+      { f: 'UP', valid: false },
+      { f: true, valid: false },
+    ];
+  
+    tests.forEach((test) => {
+      const expected = test.valid;
+      const actual = validateInput(0, 0, test.f).valid;
+      expect(actual).toBe(expected);
+    });
+  });
+  
 });
--- src/robot.test.js
+++ src/robot.test.js
@@ -1,122 +1,153 @@
 const Robot = require('./robot');
 
-test('Robot should have a valid ID', () => {
-  const robot = new Robot();
-  const regex = /[A-Z]{2}\d{3}/;
+describe('.id', () => {
 
-  const expected = true;
-  const actual = regex.test(robot.id);
-  expect(actual).toBe(expected);
-
-  const expectedlength = 5;
-  const actualLength = robot.id.length;
-  expect(actualLength).toBe(expectedlength);
-});
-
-test('.id should return the same ID when called repeatedly', () => {
-  const robot = new Robot();
-  expect(robot.id).toBe(robot.id);
-  expect(robot.id).toBe(robot.id);
-});
-
-test('REPORT should return the robot\'s current position', () => {
-  const robot = new Robot();
-  robot.PLACE(0, 0, 'NORTH');
-
-  const expected = [0, 0, 'NORTH'];
-  const actual = robot.REPORT();
-
-  expect(actual).toEqual(expected);
-});
-
-test('PLACE should be the first command', () => {
-  const robot = new Robot();
-
-  expect(() => {
-    robot.REPORT();
-  }).toThrowError();
-});
-
-test('MOVE should move the robot one unit in the direction it\'s facing', () => {
-  const tests = [
-    { origin: [0, 0, 'NORTH'], expected: [0, 1, 'NORTH'] },
-    { origin: [0, 4, 'SOUTH'], expected: [0, 3, 'SOUTH'] },
-    { origin: [0, 0, 'EAST'], expected: [1, 0, 'EAST'] },
-    { origin: [4, 0, 'WEST'], expected: [3, 0, 'WEST'] },
-  ];
-
-  tests.forEach((test) => {
+  test('should be a valid ID', () => {
     const robot = new Robot();
-    robot.PLACE(...test.origin);
+    const regex = /[A-Z]{2}\d{3}/;
+  
+    const expected = true;
+    const actual = regex.test(robot.id);
+    expect(actual).toBe(expected);
+  
+    const expectedlength = 5;
+    const actualLength = robot.id.length;
+    expect(actualLength).toBe(expectedlength);
+  });
+  
+  test('should return the same ID when called repeatedly', () => {
+    const robot = new Robot();
+    expect(robot.id).toBe(robot.id);
+    expect(robot.id).toBe(robot.id);
+  });
+
+});
+
+describe('.report()', () => {
+
+  test('should return the robot\'s current position', () => {
+    const robot = new Robot();
+    robot.PLACE(0, 0, 'NORTH');
+  
+    const expected1 = [0, 0, 'NORTH'];
+    const actual1 = robot.REPORT();
+  
+    expect(actual1).toEqual(expected1);
+
     robot.MOVE();
 
-    const expected = test.expected;
-    const actual = robot.REPORT();
-
-    expect(actual).toEqual(expected);
+    const expected2 = [0, 1, 'NORTH'];
+    const actual2 = robot.REPORT();
+  
+    expect(actual2).toEqual(expected2);
   });
+
 });
 
-test('MOVE should not allow the robot to fall off the table', () => {
-  const tests = [
-    [0, 4, 'NORTH'],
-    [0, 0, 'SOUTH'],
-    [4, 0, 'EAST'],
-    [0, 0, 'WEST'],
-  ];
+describe('.place()', () => {
 
-  tests.forEach((test) => {
+  test('should be the first command', () => {
     const robot = new Robot();
-    robot.PLACE(...test);
-    robot.MOVE();
-
-    const expected = test;
-    const actual = robot.REPORT();
-
-    expect(actual).toEqual(expected);
+  
+    expect(() => {
+      robot.REPORT();
+    }).toThrowError();
   });
+
 });
 
-test('LEFT should turn the robot to the left', () => {
-  const tests = [
-    { origin: 'NORTH', expected: 'WEST' },
-    { origin: 'SOUTH', expected: 'EAST' },
-    { origin: 'EAST', expected: 'NORTH' },
-    { origin: 'WEST', expected: 'SOUTH' },
-  ];
+describe('.move()', () => {
 
-  tests.forEach((test) => {
-    const robot = new Robot();
-
-    robot.PLACE(0, 0, test.origin);
-    robot.LEFT();
-
-    const expected = [0, 0, test.expected];
-    const actual = robot.REPORT();
-
-    expect(actual).toEqual(expected);
+  test('should move the robot one unit in the direction it\'s facing', () => {
+    const tests = [
+      { origin: [0, 0, 'NORTH'], expected: [0, 1, 'NORTH'] },
+      { origin: [0, 4, 'SOUTH'], expected: [0, 3, 'SOUTH'] },
+      { origin: [0, 0, 'EAST'], expected: [1, 0, 'EAST'] },
+      { origin: [4, 0, 'WEST'], expected: [3, 0, 'WEST'] },
+    ];
+  
+    tests.forEach((test) => {
+      const robot = new Robot();
+      robot.PLACE(...test.origin);
+      robot.MOVE();
+  
+      const expected = test.expected;
+      const actual = robot.REPORT();
+  
+      expect(actual).toEqual(expected);
+    });
   });
+  
+  test('should not allow the robot to fall off the table', () => {
+    const tests = [
+      [0, 4, 'NORTH'],
+      [0, 0, 'SOUTH'],
+      [4, 0, 'EAST'],
+      [0, 0, 'WEST'],
+    ];
+  
+    tests.forEach((test) => {
+      const robot = new Robot();
+      robot.PLACE(...test);
+      robot.MOVE();
+  
+      const expected = test;
+      const actual = robot.REPORT();
+  
+      expect(actual).toEqual(expected);
+    });
+  });
+
 });
 
-test('RIGHT should turn the robot to the right', () => {
-  const tests = [
-    { origin: 'NORTH', expected: 'EAST' },
-    { origin: 'SOUTH', expected: 'WEST' },
-    { origin: 'EAST', expected: 'SOUTH' },
-    { origin: 'WEST', expected: 'NORTH' },
-  ];
+describe('.left()', () => {
 
-  tests.forEach((test) => {
-    const robot = new Robot();
-
-    robot.PLACE(0, 0, test.origin);
-    robot.RIGHT();
-
-    const expected = [0, 0, test.expected];
-    const actual = robot.REPORT();
-
-    expect(actual).toEqual(expected);
+  test('should turn the robot to the left', () => {
+    const tests = [
+      { origin: 'NORTH', expected: 'WEST' },
+      { origin: 'SOUTH', expected: 'EAST' },
+      { origin: 'EAST', expected: 'NORTH' },
+      { origin: 'WEST', expected: 'SOUTH' },
+    ];
+  
+    tests.forEach((test) => {
+      const robot = new Robot();
+  
+      robot.PLACE(0, 0, test.origin);
+      robot.LEFT();
+  
+      const expected = [0, 0, test.expected];
+      const actual = robot.REPORT();
+  
+      expect(actual).toEqual(expected);
+    });
   });
+
+});
+
+describe('.right()', () => {
+
+  test('should turn the robot to the right', () => {
+    const tests = [
+      { origin: 'NORTH', expected: 'EAST' },
+      { origin: 'SOUTH', expected: 'WEST' },
+      { origin: 'EAST', expected: 'SOUTH' },
+      { origin: 'WEST', expected: 'NORTH' },
+    ];
+  
+    tests.forEach((test) => {
+      const robot = new Robot();
+  
+      robot.PLACE(0, 0, test.origin);
+      robot.RIGHT();
+  
+      const expected = [0, 0, test.expected];
+      const actual = robot.REPORT();
+  
+      expect(actual).toEqual(expected);
+    });
+  });
+
 });
 
 test('Should pass CONSTRAINT #1', () => {
